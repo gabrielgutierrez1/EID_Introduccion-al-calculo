f541475b6a50b6692b3b1eea07fee36f7cc5db3d
Ailyn Melillan <amelillan2025@alu.uct.cl>
2026-06-24 20:47:42 -0400

    Ejecutar interfaz grafica desde main

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from src.conicas import analizar_conica
+from gui.gui import iniciar_interfaz
 from src.rut import FORMATO_RUT, obtener_digitos, validar_rut
 from src.salida import mostrar_conica, mostrar_validacion
 
 
-def main():
+def main_consola():
     print(FORMATO_RUT)
     rut_usuario = input("Ingresa un RUT chileno: ")
     resultado_validacion = validar_rut(rut_usuario)
@@ -15,6 +16,10 @@ def main():
     digitos = obtener_digitos(resultado_validacion["cuerpo"])
     resultado_conica = analizar_conica(digitos, resultado_validacion["dv_ingresado"])
     mostrar_conica(resultado_conica)
+
+
+def main():
+    iniciar_interfaz()
 
 
 if __name__ == "__main__":
